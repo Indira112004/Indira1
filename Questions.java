9232f9627699051dce858ede541a5df798435b6b
Indira112004 <120215040+Indira112004@users.noreply.github.com>
2022-12-10 15:52:02 +0600

    Create Questions.java

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Indira");
+        System.out.println("Surname: Ashimova");
+        System.out.println("Id: 220103380");
+        System.out.println("Hobby: reading");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 8");
+        System.out.println("My favorite color?: red");
+    }
+}
